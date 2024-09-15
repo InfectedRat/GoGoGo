7595e3c0c121f051946c78c15eea99cc4e62565b
Maksim Babichev <birdclock@gmail.com>
2024-09-15 22:06:53 +0300

    test

---
+++ java-test/Calc.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; 
+
+public class Calc {
+    public void main(String[] args) {
+        System scanner = new Scanner(System.in)
+        
+    }
+}
--- java-test/VariableExample.java
+++
@@ -1,9 +0,0 @@
-public class VariableExample {
-    public static void main(String[] args) {
-        byte byteVar = 100;
-
-        System.out.println("byte = " + byteVar);
-
-    }
-    
-}
