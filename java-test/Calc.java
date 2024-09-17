3a5c61e53de243380d3522e0f88921d5eb2457da
Maksim Babichev <birdclock@gmail.com>
2024-09-17 20:35:19 +0300

    test

--- java-test/Calc.java
+++ java-test/Calc.java
@@ -1,16 +1,2 @@
 import java.util.Scanner; 
 
-public class Calc {
-    public void main(String[] args) {
-        System scanner = new Scanner(System.in)
-        
-    }
-}
-
-System.out.print("Введите ваш возраст: ");
-int age = scanner.nextInt();
-
-scanner.nextLine(); // Очистка буфера
-
-System.out.print("Введите ваше имя: ");
-String name = scanner.nextLine();
